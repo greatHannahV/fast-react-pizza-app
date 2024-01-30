4a7eb0b51d0b6bb56ece0d533bbffcd943d9f994
greatHannahV <i.kasl@mail.ru>
2024-01-30 23:16:57 +0200

    outlet

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Company</Link>
+    </header>
+  )
+}
+
+export default Header
