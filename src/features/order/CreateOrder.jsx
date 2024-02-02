b951d224acf41983e56ed5ea959b488ecaeae822
greatHannahV <i.kasl@mail.ru>
2024-02-02 17:52:24 +0200

    colors transition border fles with tailwind

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,44 +1,44 @@
-import { useState } from 'react'
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
-import { createOrder } from '../../services/apiRestaurant'
+import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
-  )
+  );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
   },
-]
+];
 
 function CreateOrder() {
-  const navigation = useNavigation()
-  const isSubmitting = navigation.state === 'submitting'
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
 
-  const formErrors = useActionData()
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart
+  const cart = fakeCart;
 
   return (
     <div>
@@ -57,7 +57,7 @@ function CreateOrder() {
             <input type="tel" name="phone" required />
           </div>
           {formErrors?.phone && (
-            <p style={{ color: 'red' }}>{formErrors.phone}</p>
+            <p style={{ color: "red" }}>{formErrors.phone}</p>
           )}
         </div>
 
@@ -81,31 +81,34 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? 'Placing order...' : 'Order now'}
+          <button
+            disabled={isSubmitting}
+            className="mt-4 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 "
+          >
+            {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
       </Form>
     </div>
-  )
+  );
 }
 export async function action({ request }) {
-  const formData = await request.formData()
-  const data = Object.fromEntries(formData)
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
 
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
-  }
-  const errors = {}
+    priority: data.priority === "on",
+  };
+  const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
-      'Please give us your correct phone number. We need it to contact you'
-  if (Object.keys(errors).length > 0) return errors
+      "Please give us your correct phone number. We need it to contact you";
+  if (Object.keys(errors).length > 0) return errors;
   //if evrething is ok, create a new order and redirect
-  const newOrder = await createOrder(order)
+  const newOrder = await createOrder(order);
 
-  return redirect(`/order/${newOrder.id}`)
+  return redirect(`/order/${newOrder.id}`);
 }
-export default CreateOrder
+export default CreateOrder;
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className=" hidden text-sm font-semibold md:block">Korban</div>;
+}
+
+export default Username;
