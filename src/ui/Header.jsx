5a0d4c24c53eb1b4d4529ca3cc31297de17a7593
greatHannahV <i.kasl@mail.ru>
2024-02-05 16:12:51 +0200

    rewrite tailwind config

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="flex items-center  justify-between border-b border-stone-200 bg-yellow-400 p-4 px-4 py-3  sm:px-6">
+    <header className=" flex  items-center justify-between border-b border-stone-200 bg-yellow-400 p-4 px-4  py-3 sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Company
       </Link>
